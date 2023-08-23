ea318b6dbdf81966e688b084d30f8f71348a5014
shima-front-dev <124478768+shima-front-dev@users.noreply.github.com>
2023-08-23 12:32:18 +0900

    Userモデルの作成など

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+    # gem bcrypt
+  has_secure_password
+
+  # validates
+  validates :name, presence: true,
+                   length: { maximum: 30, allow_blank: true }
+                   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                        length: { minimum: 8 },
+                        format: {
+                            with: VALID_PASSWORD_REGEX
+                        },
+                        allow_nil: true
+end
