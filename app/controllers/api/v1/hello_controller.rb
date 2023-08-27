73b5b08536d846cab41f0f929d17d37604068f3f
shima-front-dev <124478768+shima-front-dev@users.noreply.github.com>
2023-08-27 11:11:43 +0900

    seedデータ投入など

--- app/controllers/api/v1/hello_controller.rb
+++ app/controllers/api/v1/hello_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::HelloController < ApplicationController
     def index
-        render json: "Hello"
+         users = User.all
+         render json: users.as_json(only: [:id, :name, :email, :created_at]) 
       end
+
+    def show
+      user = User.find(params[:id])
+      p '-----------------------------'
+      p params
+      render json: user
+    end
+
 end
